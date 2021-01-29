a9002d68ac44cf7b409f34a09c15a4a65b82b6ec
Kristina Ushakova <kristina.ushakova@outlook.com>
2021-01-29 11:49:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,20 @@
 import telebot
-from config import token,user_id
+from boto.s3.connection import S3Connection
 
+token = S3Connection(os.environ['TOKEN'])
 bot = telebot.TeleBot(token)
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(user_id, "A human?!")
-
+    bot.send_message(message.from_user.id, "A human?!")
+    
 @bot.message_handler(content_types=['text'])
 def send_greetings(message):
     if "hello" in message.text.lower() or "hi" in message.text.lower():
-        bot.send_message(user_id, "Hello you!")
+        bot.send_message(message.from_user.id, "Hello you!")
     else:
-        bot.send_message(user_id, "Type \'Hello\' to begin")
+        bot.send_message(message.from_user.id, "Type \'Hello\' to begin")
 
 
 bot.polling()
