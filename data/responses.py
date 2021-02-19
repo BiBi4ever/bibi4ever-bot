35c4c48573f1c56fc16f436121abf1994220e861
xtinaushakova <kristina.ushakova@outlook.com>
2021-02-19 12:51:08 +0300

    Moved utilities to utils

---
+++ data/responses.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+snowflake = u'\U00002744'
+
+responses = {
+"hi": "Hello you!",
+"hello": "Hello you!"
+}
+
+default_response = "I don't understand you :( Please type in the /help command to see what I can do"
+start_response = "A human?! I am so happy! Please type in the /help command to see what I can do"
+help_response = f"""Here's a list of available commands: \n
+                {snowflake} /help - displays this message\n
+                {snowflake} /start - displays start message\n
+                {snowflake} /sheet_link - shows link to sheet\n
+                {snowflake} /sheet_display - shows all rows in sheet"""
+
+
+responses = defaultdict(lambda: default_response, responses)
---
+++ utils/auth.py
@@ -0,0 +1,8 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def launch_google_client(google_token):
+    scope = ['https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(google_token, scope)
+    client = gspread.authorize(creds)
+    return gspread.authorize(creds)
